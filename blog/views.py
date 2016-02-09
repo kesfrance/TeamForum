06b7b3cf2f05ea8b28c018ab37cd82bb354c60e8
Francis Kessie <kesfrance@yahoo.com>
2016-02-09 08:50:18 +0800

    added bootstrap

--- blog/views.py
+++ blog/views.py
@@ -11,6 +11,14 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flask.ext.mail import Mail, Message
 
 
+@app.route("/programs")
+def programs():
+    return render_template("programs.html")
+
+@app.route("/program/<int:id>") 
+def single_program(id):
+    return render_template('singleprogram.html', id=id)
+
 @app.route("/post")
 @app.route("/page/<int:page>")
 @login_required
@@ -39,7 +47,6 @@ def posts(page=1, paginate_by=5):
         total_pages=total_pages,
         admin = admin
     )
-
 @app.route("/")
 def main():
     return redirect(url_for("login_get"))
@@ -47,7 +54,7 @@ def main():
 @app.route("/login", methods=["GET"])
 def login_get():
     return render_template("login.html")
-    
+
 @app.route("/login", methods=["POST"])
 def login_post():
     email = request.form["email"]
@@ -75,7 +82,7 @@ def logout():
 @login_required
 def add_post_get():
     return render_template("add_post.html")
-    
+
 
 @app.route("/post/add", methods=["POST"])
 @login_required
@@ -83,7 +90,7 @@ def add_post_post():
     members = session.query(User).all()
     post = Post(
         title=request.form["title"],
-        content=request.form["content"],
+        content=mistune.markdown(request.form["content"]),
         description = request.form["description"],
         author=current_user
     )
@@ -94,7 +101,7 @@ def add_post_post():
         all_members.append(member.email)
     all_members = [str(i) for i in all_members]
     describe = request.form["description"] +".... View full message on TeamForum!"
-    mail=Mail(app) 
+    mail=Mail(app)
     with mail.connect() as conn:
       for email in all_members:
         message = Message(subject="A new forum has been created by "+ current_user.name,
@@ -106,22 +113,22 @@ def add_post_post():
         conn.send(message)
 
     flash("You have created a new forum. Team members have been notified.", "info")
-    return redirect(url_for("posts"))    
+    return redirect(url_for("posts"))
 
 #view for single post
 @app.route("/post/<int:id>", methods=['GET'])
 def singlepost_get(id):
     try:
-        post = session.query(Post).filter_by(id=id).one()    
+        post = session.query(Post).filter_by(id=id).one()
         comments = session.query(Comment).filter_by(post_id=post.id)
         comments = comments.order_by(Comment.datetime.desc()).all()
         admin = session.query(User).filter_by(email="kesfrance@yahoo.com").one()
-    
+
         return render_template("singlepost.html",
-                           post=post, 
-                           id=id, 
+                           post=post,
+                           id=id,
                            comments=comments,
-                           admin = admin                          
+                           admin = admin
                           )
     except:
           error_msg = "Sorry, the page you are trying to view doesn't exist."
@@ -131,18 +138,18 @@ def singlepost_get(id):
 @login_required
 def singlepost_comment(id):
     post = session.query(Post).filter_by(id=id).one()
-    comments = session.query(Comment).filter_by(post_id=post.id).all() 
-    
+    comments = session.query(Comment).filter_by(post_id=post.id).all()
+
     #mail list of people who commented on a forum, dont include comment author
     participants = list(set([str(i.author.email) for i in comments
                             if i.author.email != current_user.email]))
-    
+
     #if the current user is not the creator of forum, add forum creator to list
     if post.author.email != current_user.email:
-        participants.append(str(post.author.email))   
-    
+        participants.append(str(post.author.email))
+
     content = request.form["content"][:30] + "....View full message on TeamForum."
-    
+
     comment = Comment(
         post=session.query(Post).get(id),
         content=mistune.markdown(request.form["content"]),
@@ -150,12 +157,12 @@ def singlepost_comment(id):
     )
     session.add(comment)
     session.commit()
-    
+
     if len(participants) != 0:
-      mail=Mail(app) 
+      mail=Mail(app)
       with mail.connect() as conn:
           for email in list(set(participants)):
-            message = Message(subject= current_user.name + " made a comment on "            
+            message = Message(subject= current_user.name + " made a comment on "
                   + post.title, body= content,
                   sender=("TeamForum", "kesfrance@gmail.com"),
                   recipients=[email]
@@ -164,10 +171,10 @@ def singlepost_comment(id):
             conn.send(message)
 
     flash("You've posted a comment. Paricipants will be notified", "info")
-  
+
     return redirect(url_for("singlepost_get", id=id))
 
-@app.route("/comment/<int:comment_id>/edit", methods=['GET', 'POST'])       
+@app.route("/comment/<int:comment_id>/edit", methods=['GET', 'POST'])
 def editComment(comment_id):
     commentToEdit= session.query(Comment).filter_by(id=comment_id).one()
     if request.method == 'POST':
@@ -180,8 +187,8 @@ def editComment(comment_id):
         return redirect(url_for('posts'))
     else:
         return render_template('editcomment .html', p=commentToEdit)
-        
-@app.route("/comment/<int:comment_id>/delete", methods=['GET', 'POST'])       
+
+@app.route("/comment/<int:comment_id>/delete", methods=['GET', 'POST'])
 def deleteComment(comment_id):
     commentToDelete = session.query(Comment).filter_by(id=comment_id).one()
     if request.method == 'POST':
@@ -189,10 +196,10 @@ def deleteComment(comment_id):
         session.commit()
         flash("You have deleted your comment", "info")
         return redirect(url_for('posts'))
-    else:       
+    else:
         return render_template('deletecomment.html', i=commentToDelete)
-        
-        
+
+
 @app.route("/post/<int:id>/edit", methods=['GET', 'POST'])
 def editpost(id):
     postToEdit= session.query(Post).filter_by(id=id).one()
@@ -206,7 +213,7 @@ def editpost(id):
         return redirect(url_for('posts'))
     else:
         return render_template('editpost.html', p=postToEdit)
-    
+
 @app.route("/post/<int:id>/delete", methods = ["GET", "POST"])
 def deletepost(id):
     postToDelete = session.query(Post).filter_by(id=id).one()
@@ -216,7 +223,7 @@ def deletepost(id):
         return redirect(url_for('posts'))
     else:
         return render_template('deletepost.html', i=postToDelete)
-        
+
 
 @app.route("/signup", methods=["GET"])
 def signup_get():
@@ -229,25 +236,26 @@ def signup_post():
     email=request.form["email"]
     password=request.form["password"]
     password_2=request.form["re-password"]
-    
+
     if session.query(User).filter_by(email=email).first():
         flash("User with that email address already exists", "danger")
         return redirect(url_for("signup_get"))
-        
+
     if not (password and password_2) or password != password_2:
         flash("Passwords did not match", "danger")
         return redirect(url_for("signup_get"))
-    
+
     user = User(name=name, email=email, password=generate_password_hash(password))
-    
+
     session.add(user)
     session.commit()
-    
+
     flash("Success! You may now login with your credentials", "info")
     return redirect(url_for("login_get"))
 
 
 @app.route("/post/JSON", methods=["GET"])
+@login_required
 def posts_get():
     """ Get a list of posts """
     posts = session.query(Post).order_by(Post.id)
@@ -257,6 +265,7 @@ def posts_get():
     return Response(data, 200, mimetype="application/json")
 
 @app.route("/post/<int:id>/JSON", methods=["GET"])
+@login_required
 def post_get(id):
     """ Single post endpoint """
     post = session.query(Post).get(id)
@@ -270,3 +279,8 @@ def post_get(id):
 
     data = json.dumps(post.as_dictionary())
     return Response(data, 200, mimetype="application/json")
+
+@app.route("/post/names/all/all")
+def query():
+   users = session.query(User).order_by(User.id)
+   return render_template("users.html", users=users)
